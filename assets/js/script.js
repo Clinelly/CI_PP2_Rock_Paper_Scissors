3f818b5f77f772f97e988d2be702a152dc0f0243
Clinelly <swall289@gmail.com>
2022-09-16 10:12:48 +0000

    Add runGame function to play button event listener.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,17 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for (let button of buttons){
         button.addEventListener("click", function(){
-            if (this.getAttribute("data-type") === "rules"){
+            if (this.getAttribute("data-type") === "rules"){ //Changes text-area html to display the game rules
                 document.getElementById("game-text").innerHTML = "The rules of the game are very simple!<br>Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard.<br> Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard.<br> Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock, and as it always has, Rock crushes Scissors.";
-            } else if (this.getAttribute("data-type") === "reset"){
+            } else if (this.getAttribute("data-type") === "reset"){ //Restores text-area html to initial value
                     document.getElementById("game-text").innerHTML = "Welcome to Rock, Paper, Scissor, Lizard, Spock!<br>Press 'Play' to start a game, or 'Rules' to read the rules!";
-                } else {
-                    let gameInput = this.getAttribute("data-type");
-                    alert(`You clicked ${gameInput}`);
+                } else if (this.getAttribute("data-type") === "play"){
+                    runGame();
             }
         })
     }
 })
+
+function runGame() {
+    document.getElementById("game-text").innerHTML = "Your Score: <br> Computer Score: <br> Choose wisely! Rock, Paper, Scissors, Lizard or Spock...";
+}
