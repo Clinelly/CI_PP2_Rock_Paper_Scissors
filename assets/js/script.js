91d84a3361025f8d0d837dbfcbe655c1921b396f
Clinelly <swall289@gmail.com>
2022-09-16 09:47:01 +0000

    Change event listener for reset button to return text-area to original state.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,11 @@ document.addEventListener("DOMContentLoaded", function(){
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "rules"){
                 document.getElementById("game-text").innerHTML = "The rules of the game are very simple!<br>Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard.<br> Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard.<br> Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock, and as it always has, Rock crushes Scissors.";
-            } else {
-                let gameInput = this.getAttribute("data-type");
-                alert(`You clicked ${gameInput}`);
+            } else if (this.getAttribute("data-type") === "reset"){
+                    document.getElementById("game-text").innerHTML = "Welcome to Rock, Paper, Scissor, Lizard, Spock!<br>Press 'Play' to start a game, or 'Rules' to read the rules!";
+                } else {
+                    let gameInput = this.getAttribute("data-type");
+                    alert(`You clicked ${gameInput}`);
             }
         })
     }
