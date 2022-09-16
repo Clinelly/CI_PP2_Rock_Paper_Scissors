4de3e959ab966ae4304de805210385ea267f04e7
Clinelly <swall289@gmail.com>
2022-09-16 09:43:16 +0000

    Change event listener for rules button to display rules text in text area.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons){
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "rules"){
-                alert("You clicked the rules!");
+                document.getElementById("game-text").innerHTML = "The rules of the game are very simple!<br>Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard.<br> Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard.<br> Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock, and as it always has, Rock crushes Scissors.";
             } else {
                 let gameInput = this.getAttribute("data-type");
                 alert(`You clicked ${gameInput}`);
