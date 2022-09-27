407f133819bcfb558c51047e70cc45d99daf35df
Clinelly <swall289@gmail.com>
2022-09-27 18:33:48 +0000

    Add more EmailJS script to form.js.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,2 +1,30 @@
-emailjs.init('ojZj8ZhHGIGV035IG');
+
+/**
+ * Add an eventListener to listen for the submit.
+ * Sends an email to site owner through emailJS if the submit is fired.
+ * Script taken from the official EmailJS tutorial https://www.emailjs.com/docs/tutorial/creating-contact-form/ 
+ * and Email Templates Playground environment.
+ * emailjs.init('ojZj8ZhHGIGV035IG');
 emailjs.sendForm('contact_service', 'contact_form', this)
+ */
+ const btn = document.getElementById('submit');
+
+ document.getElementById('email-form')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
+    emailjs.init('ojZj8ZhHGIGV035IG');
+ 
+    btn.value = 'Sending...';
+ 
+    const serviceID = 'default_service';
+    const templateID = 'contact_form';
+ 
+    emailjs.sendForm(serviceID, templateID, this)
+     .then(() => {
+       btn.value = 'Send';
+       
+     }, (err) => {
+       btn.value = 'Send';
+       alert(JSON.stringify(err));
+     });
+ });
