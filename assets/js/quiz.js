8dd1cfecc4234885b0d75e26fd536d740f734dae
Clinelly <swall289@gmail.com>
2022-09-28 21:04:14 +0000

    Add script to quiz.js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,4 @@
-let quizQuestions = [ //Generates the array of questions.
+let myQuestions = [ //Generates the array of questions.
     {
         question: "How do lizards smell?",
         answers: {
@@ -105,7 +105,7 @@ let quizQuestions = [ //Generates the array of questions.
             b: "Klingon.",
             c: "Vulcan."
         },
-        correctAnswer: "a"
+        correctAnswer: "c"
     },
     {
         question: "What is a 'Rocky Road'?",
@@ -136,16 +136,90 @@ let quizQuestions = [ //Generates the array of questions.
     },
 ]
 
-function generateQuiz(){
+var quizContainer = document.getElementById('quiz-area');
+var resultsContainer = document.getElementById('answer-area');
+var submitButton = document.getElementById('quiz-submit');
 
-    function showQuestions(){
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+  function showQuestions(questions, quizContainer){
+    // we'll need a place to store the output and the answer choices
+    var output = [];
+    var answers;
+
+    // for each question...
+    for(var i=0; i<questions.length; i++){
+      
+      // first reset the list of answers
+      answers = [];
+
+      // for each available answer...
+      for(letter in questions[i].answers){
+
+        // ...add an html radio button
+        answers.push(
+          '<label>'
+            + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+            + letter + ': '
+            + questions[i].answers[letter]
+          + '</label>'
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        '<div class="question">' + questions[i].question + '</div>'
+        + '<div class="answers">' + answers.join('') + '</div>'
+      );
     }
 
-    function showResults() {
+    // finally combine our output list into one string of html and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
 
+
+  function showResults(questions, quizContainer, resultsContainer){
+    
+    // gather answer containers from our quiz
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    
+    // keep track of user's answers
+    var userAnswer = '';
+    var numCorrect = 0;
+    
+    // for each question...
+    for(var i=0; i<questions.length; i++){
+
+      // find selected answer
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      
+      // if answer is correct
+      if(userAnswer===questions[i].correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+        
+        // color the answers green
+        answerContainers[i].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[i].style.color = 'red';
+      }
     }
 
-    showQuestions();
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+  }
+
+  // show questions right away
+  showQuestions(questions, quizContainer);
+  
+  // on submit, show results
+  submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+  }
 
 }
