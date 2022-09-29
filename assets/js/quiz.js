c4ae70a40c48dfc47fa5b071b32df591981dcabd
Clinelly <swall289@gmail.com>
2022-09-29 17:20:12 +0000

    Change quiz.js to change layout and functionality of quiz.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,14 @@
-let myQuestions = [ //Generates the array of questions.
+//Waits for DOM to fully load before running script.
+//Gets button elements and adds event-listeners to them.
+document.addEventListener("DOMContentLoaded", function(){
+    let start = document.getElementById("btn-start");
+    start.addEventListener("click", function(){
+        startQuiz();
+        })
+    })
+            
+// Array of questions
+let questions = [ 
     {
         question: "How do lizards smell?",
         answers: {
@@ -136,90 +146,53 @@ let myQuestions = [ //Generates the array of questions.
     },
 ]
 
-var quizContainer = document.getElementById('quiz-area');
-var resultsContainer = document.getElementById('answer-area');
-var submitButton = document.getElementById('quiz-submit');
+function startQuiz () {
+    let questionArea = document.getElementById("quiz-area")
 
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+    questionArea.innerHTML = ""
 
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-  function showQuestions(questions, quizContainer){
-    // we'll need a place to store the output and the answer choices
-    var output = [];
-    var answers;
-
-    // for each question...
-    for(var i=0; i<questions.length; i++){
-      
-      // first reset the list of answers
-      answers = [];
-
-      // for each available answer...
-      for(letter in questions[i].answers){
-
-        // ...add an html radio button
-        answers.push(
-          '<label>'
-            + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-            + letter + ': '
-            + questions[i].answers[letter]
-          + '</label>'
-        );
-      }
-
-      // add this question and its answers to the output
-      output.push(
-        '<div class="question">' + questions[i].question + '</div>'
-        + '<div class="answers">' + answers.join('') + '</div>'
-      );
-    }
-
-    // finally combine our output list into one string of html and put it on the page
-    quizContainer.innerHTML = output.join('');
-  }
-
-
-  function showResults(questions, quizContainer, resultsContainer){
+    let previousButton = document.createElement('button');
+    let nextButton = document.createElement('button');
     
-    // gather answer containers from our quiz
-    var answerContainers = quizContainer.querySelectorAll('.answers');
-    
-    // keep track of user's answers
-    var userAnswer = '';
-    var numCorrect = 0;
-    
-    // for each question...
-    for(var i=0; i<questions.length; i++){
+    previousButton.setAttribute('id', 'btn-pre');
+    previousButton.setAttribute('class', 'btn');
+    previousButton.setAttribute('class', 'btn--sec');
+    previousButton.innerHTML = "Previous<br>Question";
+    nextButton.setAttribute('id','btn-nxt');
+    nextButton.setAttribute('class','btn');
+    nextButton.setAttribute('class', 'btn--sec');
+    nextButton.innerHTML = "Next<br>Question"
 
-      // find selected answer
-      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-      
-      // if answer is correct
-      if(userAnswer===questions[i].correctAnswer){
-        // add to the number of correct answers
-        numCorrect++;
-        
-        // color the answers green
-        answerContainers[i].style.color = 'lightgreen';
-      }
-      // if answer is wrong or blank
-      else{
-        // color the answers red
-        answerContainers[i].style.color = 'red';
-      }
-    }
+    document.getElementById("quiz-area").appendChild(previousButton)
+    document.getElementById("quiz-area").appendChild(nextButton)
 
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-  }
-
-  // show questions right away
-  showQuestions(questions, quizContainer);
-  
-  // on submit, show results
-  submitButton.onclick = function(){
-    showResults(questions, quizContainer, resultsContainer);
-  }
-
+    generateQuestions(questionArea);
 }
+
+function generateQuestions(questionArea) {
+    let output = [];
+    let answers;
+        for (let i=0; i<questions.length; i++){
+            answers = [];
+            for (letter in questions[0].answers){
+                
+				// ...add an html radio button
+				answers.push(
+					'<label>'
+						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+						+ letter + ': '
+						+ questions[0].answers[letter]
+					+ '</label>'
+				);
+			}
+
+			// add this question and its answers to the output
+			output.push(
+				'<div class="question">' + questions[0].question + '</div>'
+				+ '<div class="answers">' + answers.join('') + '</div>'
+			);
+		}
+
+		// finally combine our output list into one string of html and put it on the page
+		questionArea.innerHTML = output.join('');
+	}
